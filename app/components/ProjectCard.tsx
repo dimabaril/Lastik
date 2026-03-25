e42135c4109a735577e2461268d3f52513226fa3
Dmitrii Barilkin <dimabaril@gmail.com>
2026-03-25 13:11:01 +0100

    webm preview

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -1,93 +1,52 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 
 interface ProjectCardProps {
   title: string;
   span: string;
-  videoId: string;
-}
-
-interface VimeoPlayer {
-  play: () => Promise<void>;
-  pause: () => Promise<void>;
-}
-
-declare global {
-  interface Window {
-    Vimeo: {
-      Player: new (iframe: HTMLIFrameElement) => VimeoPlayer;
-    };
-  }
+  thumbVideo: string;
 }
 
 export default function ProjectCard({
   title,
   span,
-  videoId,
+  thumbVideo,
 }: ProjectCardProps) {
-  const iframeRef = useRef<HTMLIFrameElement | null>(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
 
-  useEffect(() => {
-    const iframe = iframeRef.current;
-    if (!iframe) return;
+  const handleMouseEnter = () => {
+    videoRef.current?.play().catch(() => {});
+  };
 
-    // const player = new (window as any).Vimeo.Player(iframe);
-    const player = new window.Vimeo.Player(iframe);
-    const container = iframe.parentElement?.parentElement;
-
-    const handleMouseEnter = () => player.play();
-    const handleMouseLeave = () => player.pause();
-    const handleTouchStart = () => player.play();
-    const handleTouchEnd = () => player.pause();
-
-    // pc
-    container?.addEventListener("mouseenter", handleMouseEnter);
-    container?.addEventListener("mouseleave", handleMouseLeave);
-
-    // mobile
-    container?.addEventListener("touchstart", handleTouchStart);
-    container?.addEventListener("touchend", handleTouchEnd);
-
-    return () => {
-      container?.removeEventListener("mouseenter", handleMouseEnter);
-      container?.removeEventListener("mouseleave", handleMouseLeave);
-      container?.removeEventListener("touchstart", handleTouchStart);
-      container?.removeEventListener("touchend", handleTouchEnd);
-    };
-  }, []);
+  const handleMouseLeave = () => {
+    const video = videoRef.current;
+    if (!video) return;
+    video.pause();
+    video.currentTime = 0;
+  };
 
   return (
     <div
-      className={`${span} relative rounded-2xl p-[10_10_48_10] border border-dashed border-transparent hover:border-white/70 overflow-hidden`}
+      className={`${span} relative rounded-2xl p-[10_10_48_10] border border-dashed border-transparent hover:border-white/70 overflow-hidden cursor-pointer hover:scale-105 transition-transform ease-in-out duration-500`}
+      //   style={{ aspectRatio: "16 / 9" }}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onTouchStart={handleMouseEnter}
+      onTouchEnd={handleMouseLeave}
     >
+      <video
+        ref={videoRef}
+        src={thumbVideo}
+        loop
+        muted
+        playsInline
+        className="w-full h-full rounded-lg object-cover"
+      />
+      <div className="absolute inset-0 bg-linear-to-t from-black/70 via-transparent to-transparent pointer-events-none" />
       <span className="absolute bottom-3 left-3 text-white/90 underline underline-offset-4">
         {title}
       </span>
-      <div
-        style={{
-          paddingBottom: "56.25%",
-          position: "relative",
-          borderRadius: "8px",
-          overflow: "hidden",
-        }}
-      >
-        <iframe
-          ref={iframeRef}
-          src={`https://player.vimeo.com/video/${videoId}?badge=0&autopause=0&controls=0&player_id=0&app_id=58479`}
-          frameBorder="0"
-          allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-          }}
-          title={title}
-        />
-      </div>
     </div>
   );
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,31 +4,160 @@ import ProjectCard from "../components/ProjectCard";
 
 const projects = [
   {
-    title: "LOVE generation",
+    title: "Love Generation",
     span: "col-span-1 row-span-1",
     videoId: "926255997",
+    thumbVideo: "/projects/love-generation/thumb/love_generation-thumb.webm",
   },
   {
-    title: "НЕО",
+    title: "VK NEO",
     span: "col-span-1 row-span-2",
     videoId: "926255997",
+    thumbVideo: "/projects/vk-neo/thumb/NEO_pw.webm",
     featured: true,
   },
   {
-    title: "СБЕР. терминал",
+    title: "Уралсиб",
     span: "col-span-1 row-span-1",
     videoId: "926255997",
+    thumbVideo: "/projects/uralsib/thumb/uralsib-thumb.webm",
   },
-  { title: "Уралсиб", span: "col-span-1 row-span-1", videoId: "926255997" },
-  { title: "ЕАПТЕКА", span: "col-span-1 row-span-1", videoId: "926255997" },
-  { title: "СБЕР1. звук", span: "col-span-1 row-span-1", videoId: "926255997" },
-  { title: "ВТБ", span: "col-span-1 row-span-1", videoId: "926255997" },
-  { title: "СБЕР2. звук", span: "col-span-1 row-span-1", videoId: "926255997" },
-  { title: "Делимобиль", span: "col-span-1 row-span-1", videoId: "926255997" },
-  { title: "СБЕР3", span: "col-span-1 row-span-1", videoId: "926255997" },
-  { title: "МТС", span: "col-span-1 row-span-1", videoId: "926255997" },
-  { title: "СБЕР4. звук", span: "col-span-1 row-span-1", videoId: "926255997" },
-  { title: "Волчок", span: "col-span-1 row-span-1", videoId: "926255997" },
+  {
+    title: "Делимобиль",
+    span: "col-span-1 row-span-1",
+    videoId: "926255997",
+    thumbVideo: "/projects/delimobil/thumb/delimobil-thumb.webm",
+  },
+  {
+    title: "МТС",
+    span: "col-span-1 row-span-1",
+    videoId: "926255997",
+    thumbVideo: "/projects/mts/thumb/mts-thumb.webm",
+  },
+  {
+    title: "Звук",
+    span: "col-span-1 row-span-1",
+    videoId: "926255997",
+    thumbVideo: "/projects/zvuk/thumb/zvuk-thumb.webm",
+  },
+  {
+    title: "Звук 2",
+    span: "col-span-1 row-span-1",
+    videoId: "926255997",
+    thumbVideo: "/projects/zvuk-2/thumb/zvuk-2-thumb.webm",
+  },
+  {
+    title: "ВТБ",
+    span: "col-span-1 row-span-1",
+    videoId: "926255997",
+    thumbVideo: "/projects/vtb-1/thumb/vtb-1-thumb.webm",
+  },
+  {
+    title: "Музей Москвы",
+    span: "col-span-1 row-span-1",
+    videoId: "926255997",
+    thumbVideo: "/projects/mosmuseum/thumb/mosmuseum-thumb.webm",
+  },
+  {
+    title: "Green Idea",
+    span: "col-span-1 row-span-1",
+    videoId: "926255997",
+    thumbVideo: "/projects/green-idea/thumb/green-idea-thumb.webm",
+  },
+  {
+    title: "Mail.ru",
+    span: "col-span-1 row-span-1",
+    videoId: "926255997",
+    thumbVideo: "/projects/mail-ru/thumb/mail-thumb.webm",
+  },
+  {
+    title: "Яндекс",
+    span: "col-span-1 row-span-1",
+    videoId: "926255997",
+    thumbVideo: "/projects/yandex-incl/thumb/ya_incl-thumb.webm",
+  },
+  {
+    title: "Волчок",
+    span: "col-span-1 row-span-1",
+    videoId: "926255997",
+    thumbVideo: "/projects/volchok/thumb/volchok-thumb.webm",
+  },
+  {
+    title: "Такси",
+    span: "col-span-1 row-span-1",
+    videoId: "926255997",
+    thumbVideo: "/projects/taxi-v2/thumb/taxi-v2-thumb.webm",
+  },
+  {
+    title: "Манси",
+    span: "col-span-1 row-span-1",
+    videoId: "926255997",
+    thumbVideo: "/projects/mansi/thumb/mansi-thumb.webm",
+  },
+  {
+    title: "Странейшие",
+    span: "col-span-1 row-span-1",
+    videoId: "926255997",
+    thumbVideo:
+      "/projects/stranneyshie-horiz/thumb/stranneyshie-horiz-thumb.webm",
+  },
+  {
+    title: "Hospitality",
+    span: "col-span-1 row-span-1",
+    videoId: "926255997",
+    thumbVideo: "/projects/hospitality/thumb/hospitality-thumb.webm",
+  },
+  {
+    title: "I want to know",
+    span: "col-span-1 row-span-1",
+    videoId: "926255997",
+    thumbVideo:
+      "/projects/i-want-to-know-everything/thumb/i-want-to-know-everything-thumb.webm",
+  },
+  {
+    title: "Presents Fest",
+    span: "col-span-1 row-span-1",
+    videoId: "926255997",
+    thumbVideo:
+      "/projects/presents-fest-2024/thumb/presents-fest-2024-thumb.webm",
+  },
+  {
+    title: "Saint Spring",
+    span: "col-span-1 row-span-1",
+    videoId: "926255997",
+    thumbVideo: "/projects/saint-spring-v3/thumb/saint-spring-v3-thumb.webm",
+  },
+  {
+    title: "SBER High Res",
+    span: "col-span-1 row-span-1",
+    videoId: "926255997",
+    thumbVideo: "/projects/sber-high-res/thumb/sber-high-res-thumb.webm",
+  },
+  {
+    title: "Собчак",
+    span: "col-span-1 row-span-1",
+    videoId: "926255997",
+    thumbVideo: "/projects/sobchak/thumb/sobchak_thumb.webm",
+  },
+  {
+    title: "Supermarket",
+    span: "col-span-1 row-span-1",
+    videoId: "926255997",
+    thumbVideo:
+      "/projects/supermarket-trollys-dream-v1/thumb/supermarket-trollys-dream-v1-thumb.webm",
+  },
+  {
+    title: "The Skin",
+    span: "col-span-1 row-span-1",
+    videoId: "926255997",
+    thumbVideo: "/projects/the-skin-v1/thumb/the-skin-v1-thumb.webm",
+  },
+  {
+    title: "Unprincipled",
+    span: "col-span-1 row-span-1",
+    videoId: "926255997",
+    thumbVideo: "/projects/unprincipled/thumb/unprincipled-thumb.webm",
+  },
 ];
 
 export default function Projects() {
@@ -79,12 +208,12 @@ export default function Projects() {
         {/* ─── Projects grid ─── */}
         <section className="px-6 pb-24 pr-32">
           <div className="grid grid-cols-3 gap-3">
-            {projects.map((project, i) => (
+            {projects.map((project) => (
               <ProjectCard
                 key={project.title}
                 title={project.title}
                 span={project.span}
-                videoId={project.videoId}
+                thumbVideo={project.thumbVideo}
               />
             ))}
           </div>
