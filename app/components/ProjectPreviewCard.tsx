6e406de29d924126809d750b0adcd59939a63d7c
Dmitrii Barilkin <dimabaril@gmail.com>
2026-04-14 21:31:03 +0200

    replace debag block wide info

--- app/components/ProjectPreviewCard.tsx
+++ app/components/ProjectPreviewCard.tsx
@@ -36,7 +36,7 @@ export default function ProjectPreviewCard({
   return (
     <Link
       href={`/projects/${slug}`}
-      className="group flex flex-col rounded-2xl border border-dashed border-transparent hover:border-(--fade-color) overflow-hidden p-3 cursor-pointer"
+      className="relative group flex flex-col rounded-2xl border border-dashed border-transparent hover:border-(--fade-color) overflow-hidden p-3 cursor-pointer"
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
       onTouchStart={handleMouseEnter}
@@ -65,7 +65,11 @@ export default function ProjectPreviewCard({
         <div className="shrink-0 font-manrope text-xl flex items-center gap-2">
           {title}
           {/* size for debug */}
-          {size && <span className="text-(--fade-color)">w{size}</span>}
+          {size && (
+            <span className="text-(--fade-color) absolute top-4 right-5">
+              debug block wide: {size}
+            </span>
+          )}
         </div>
         <div className="flex gap-1 font-victor-mono text-lg text-(--fade-color) opacity-0 group-hover:opacity-100 transition-all duration-300">
           {tags.map((tag) => (
