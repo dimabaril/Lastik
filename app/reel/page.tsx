a480d970c32df59216e6590a8f1279e8ade0e6e7
Dmitrii Barilkin <dimabaril@gmail.com>
2026-04-24 10:07:39 +0200

    Revert "reel size full screen"
    This reverts commit 3efe57f6a668132ac1576be344fd52167a3caf64.

--- app/reel/page.tsx
+++ app/reel/page.tsx
@@ -4,7 +4,7 @@ const VIMEO_REEL_ID = 1073893317;
 
 export default function ArtCourse() {
   return (
-    <div className="flex flex-col flex-1 w-full bg-black text-white items-center justify-center p-6">
+    <div className="flex flex-col flex-1 w-full bg-black text-white items-center justify-center max-w-screen-xl mx-auto p-6">
       <h1 className="sr-only">Reel</h1>
       <VimeoPlayer videoId={VIMEO_REEL_ID} />
     </div>
