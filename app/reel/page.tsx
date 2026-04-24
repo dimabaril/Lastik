2dc980fbddd11a489c8ab2f3970da8b5cea08c0f
Dmitrii Barilkin <dimabaril@gmail.com>
2026-04-24 09:59:18 +0200

    minor fix

--- app/reel/page.tsx
+++ app/reel/page.tsx
@@ -1,13 +1,12 @@
 import VimeoPlayer from "@/app/components/VimeoPlayer";
 
-// 1073893317
+const VIMEO_REEL_ID = 1073893317;
 
 export default function ArtCourse() {
   return (
     <div className="flex flex-col flex-1 w-full bg-black text-white items-center justify-center max-w-screen-xl mx-auto p-6">
       <h1 className="sr-only">Reel</h1>
-
-      <VimeoPlayer videoId={1073893317} />
+      <VimeoPlayer videoId={VIMEO_REEL_ID} />
     </div>
   );
 }
