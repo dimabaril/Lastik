34b0db778991e67b2bf7020d4de2ca612f0dd895
Dmitrii Barilkin <dimabaril@gmail.com>
2026-05-04 15:13:02 +0200

    fix routing default

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/",
-        destination: "/about",
+        destination: "/projects",
         permanent: false,
       },
     ];
