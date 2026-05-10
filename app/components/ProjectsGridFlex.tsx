7a7af2c52f5a39558d4a73ba447944757201c25d
Dmitrii Barilkin <dimabaril@gmail.com>
2026-05-11 01:43:57 +0200

    Revert "fix mobile layout items-start -> items-stretch"
    This reverts commit 4487c34d43715e785f3e71e517bbcff8bed65516.

--- app/components/ProjectsGridFlex.tsx
+++ app/components/ProjectsGridFlex.tsx
@@ -72,7 +72,7 @@ export default function ProjectsGridFlex({
   return (
     <div ref={gridRef} className="flex flex-col">
       {rows.map((row, rowIndex) => (
-        <div key={rowIndex} className="flex items-stretch max-[900px]:flex-col">
+        <div key={rowIndex} className="flex items-start max-[900px]:flex-col">
           {row.map((project, colIndex) => {
             const size = project.size ?? 5;
             const isHovered =
